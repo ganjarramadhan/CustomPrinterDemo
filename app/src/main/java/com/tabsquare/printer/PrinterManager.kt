505cb05ac5c2f4d1228891e7b7faab06f193e936
Ganjar Ramadhan <ganjar.ramadhan05@gmail.com>
2020-11-11 10:21:40 +0700

    - update max char for senor

--- app/src/main/java/com/tabsquare/printer/PrinterManager.kt
+++ app/src/main/java/com/tabsquare/printer/PrinterManager.kt
@@ -13,7 +13,7 @@ object PrinterManager {
         activity: AppCompatActivity,
         printerRequest: PrinterRequest
     ): CorePrinter {
-        val receiptTemplate = DefaultReceiptTemplate(printerRequest, maxChar = 45)
+        val receiptTemplate = DefaultReceiptTemplate(printerRequest, maxChar = 47)
         // return TabsquareCustomPrinter(context = activity, receiptTemplate = receiptTemplate)
         return SenorPrinter(context = activity, receiptTemplate = receiptTemplate)
     }
